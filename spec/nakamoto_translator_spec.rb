a6e999a3a08a7ab0cb25000dd83ad7c2aa3693ff
Rasmus Kjellberg <2277443+kjellberg@users.noreply.github.com>
2024-02-22 00:03:21 +0100

    translate from nakamoto to japanese

--- spec/nakamoto_translator_spec.rb
+++ spec/nakamoto_translator_spec.rb
@@ -1,11 +1,15 @@
 # frozen_string_literal: true
 
 RSpec.describe NakamotoTranslator do
-  it "encodes from latin to nakamoto (Hal Finey → サトし なカモと)" do
+  it "translates from latin to nakamoto (Hal Finey → サトし なカモと)" do
     expect(NakamotoTranslator.from_latin_to_nakamoto("hal finey")).to eq("サトし なカモと")
   end
 
-  it "encodes from nakamoto to latin (サトし なカモと → hal finey)" do
+  it "translates from nakamoto to latin (サトし なカモと → hal finey)" do
     expect(NakamotoTranslator.from_nakamoto_to_latin("サトし なカモと")).to eq("hal finey")
+  end
+
+  it "translates from nakamoto to japanese (サトし なカモと → satoshi nakamoto)" do
+    expect(NakamotoTranslator.from_nakamoto_to_japanese("サトし なカモと")).to eq("satoshi nakamoto")
   end
 end
